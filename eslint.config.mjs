0410979a5c29744ce661412a7a1925916e78fe33
Tore Mæland Hirth <torehirth@gmail.com>
2025-03-04 13:00:18 +0100

    fix eslint error by add ...globals.node to eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,8 @@ export default [
     languageOptions: {
       globals: {
         ...globals.browser,
+        ...globals.node, // This adds Node.js global variables, including `global`
+        global: true, // Explicitly allow `global
         describe: true, // Used for grouping tests
         test: true, // Used to create tests
         it: true, // Alternative way to create tests

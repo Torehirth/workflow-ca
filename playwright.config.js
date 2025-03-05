e754a91daf12c8ec2c02644ae8373d258d90f9f7
Tore Mæland Hirth <torehirth@gmail.com>
2025-03-05 09:22:51 +0100

    update playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
-    baseURL: "https://wwww.torehirth.github.io/workflow-ca/",
+    baseURL: "http://localhost:5500",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -73,9 +73,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "npm run start",
+    url: "http://localhost:5500",
+    reuseExistingServer: !process.env.CI,
+  },
 });

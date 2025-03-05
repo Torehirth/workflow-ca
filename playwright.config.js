f82990b06399bb165fd8724538c503e5ba843ce0
Tore Mæland Hirth <torehirth@gmail.com>
2025-03-05 09:15:26 +0100

    add base URL to playwright config file

--- playwright.config.js
+++ playwright.config.js
@@ -28,6 +28,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
+    baseURL: "https://wwww.torehirth.github.io/workflow-ca/",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",

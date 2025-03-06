66555457be36fb168773178202124d2be5f544d9
Tore Mæland Hirth <torehirth@gmail.com>
2025-03-06 11:04:37 +0100

    Exclude Vitest from tests folder

--- vitest.config.js
+++ vitest.config.js
@@ -3,5 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   test: {
     environment: "jsdom",
+    //  Tell Vitest to exclude the Playwright tests folder
+    exclude: ["**/node_modules/**", "**/tests/**"], // This assumes ONLY the Playwright tests are in the tests folder.
   },
 });
